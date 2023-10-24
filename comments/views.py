f4ba105ba71fa43835cfe32efda22eef226d149a
HMuraja <hilla.muraja@hotmail.com>
2023-10-24 20:05:11 +0000

    Fix bug Add filterset field for reviews.

--- comments/views.py
+++ comments/views.py
@@ -12,6 +12,7 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filterset_fields = ['review']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
